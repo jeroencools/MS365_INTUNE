d7d3d9f9664572ab8b971fb02431c6b3218fe7ee
Jeroen Cools <113233490+jeroencools@users.noreply.github.com>
2023-01-15 13:10:50 +0100

    Add files via upload

---
+++ add url shortcut.ps1
@@ -0,0 +1,4 @@
+﻿$Shell = New-Object -ComObject ("WScript.Shell")
+$Favorite = $Shell.CreateShortcut($env:USERPROFILE + "\Desktop\NAMEOFSHORTCUT.url")
+$Favorite.TargetPath = "LINK TO WEBSITE"
+$Favorite.Save()
