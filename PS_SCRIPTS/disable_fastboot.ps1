d1cde233949344112355de40a643fdbeeaf659f9
Jeroen Cools <113233490+jeroencools@users.noreply.github.com>
2023-12-23 10:50:36 +0100

    Update disable_fastboot.ps1

--- PS_SCRIPTS/disable_fastboot.ps1
+++ PS_SCRIPTS/disable_fastboot.ps1
@@ -1,1 +1,5 @@
+<#
+A simple script that disables (or enables) fastboot. This was used because there were some issues with our wireless adapter drivers when fastboot was enabled.
+#>
+
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d "0" /f
