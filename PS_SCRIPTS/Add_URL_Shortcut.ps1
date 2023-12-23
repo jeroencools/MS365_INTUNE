3e21f593d48f3e697eb1f55614dc12484b8f17e4
Jeroen Cools <113233490+jeroencools@users.noreply.github.com>
2023-12-23 10:49:25 +0100

    Update Add_URL_Shortcut.ps1

--- PS_SCRIPTS/Add_URL_Shortcut.ps1
+++ PS_SCRIPTS/Add_URL_Shortcut.ps1
@@ -1,4 +1,10 @@
-﻿$Shell = New-Object -ComObject ("WScript.Shell")
+<#
+You can quickly add a shortcut (on desktop by default) to a certain website by usin this script. 
+The only values you need to adjust is the name of the shortcut and the link to the website. 
+Of course you can always choose another path to add a shortcut or add more than one by changing the script.
+#>
+ 
+$Shell = New-Object -ComObject ("WScript.Shell")
 $Favorite = $Shell.CreateShortcut($env:USERPROFILE + "\Desktop\NAMEOFSHORTCUT.url")
 $Favorite.TargetPath = "LINK TO WEBSITE"
 $Favorite.Save()
