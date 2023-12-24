e506550c8dae40d016f33adc7efb1dbe8f30faa3
Jeroen Cools <113233490+jeroencools@users.noreply.github.com>
2023-12-24 23:34:56 +0100

    Create Set_Wallpaper.ps1

---
+++ PS_SCRIPTS/Personalization/Set_Wallpaper.ps1
@@ -0,0 +1,39 @@
+<#
+Credits: TheWhiteZombie (https://www.reddit.com/user/TheWhiteZombie/)
+
+A script that changes the desktop wallpaper. 
+You change the $url and the script downloads the imge to a $directory that you choose.
+If the directory does not exit - it will be created.
+Afterwards the image is used to change the wallpaper.
+#>
+
+
+$RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+$DesktopPath = "DesktopImagePath"
+$DesktopStatus = "DesktopImageStatus"
+$DesktopUrl = "DesktopImageUrl"
+$StatusValue = "1"
+$url = "DIRECT_LINK_TO_IMAGE"
+$DesktopImageValue = "C:\mdm\wallpaper\1.jpg"
+$directory = "C:\mdm\wallpaper"
+
+If ((Test-Path -Path $directory) -eq $false)
+{
+	New-Item -Path $directory -ItemType directory
+}
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $DesktopImageValue)
+
+if (!(Test-Path $RegKeyPath))
+{
+	Write-Host "Creating registry path $($RegKeyPath)."
+	New-Item -Path $RegKeyPath -Force | Out-Null
+}
+
+New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $Statusvalue -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+
+RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters 1, True
+
