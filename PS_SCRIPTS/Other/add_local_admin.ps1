97dd00a3e772b73f4f48d9f0e90b63672b127c9a
Jeroen Cools <113233490+jeroencools@users.noreply.github.com>
2024-01-22 19:13:54 +0100

    Create add_local_admin.ps1

---
+++ PS_SCRIPTS/Other/add_local_admin.ps1
@@ -0,0 +1,8 @@
+# execute on device locally by elevating a PowerShell session
+$user = (Get-WmiObject -Class Win32_ComputerSystem).UserName.Split('\')[1]
+net localgroup Administrators /add "AzureAD\$user"
+
+################
+
+# can be executed through Intune admin center
+net localgroup Administrators /add "AzureAD\<UPN>"
